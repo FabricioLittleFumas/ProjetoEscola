b3c9c2f83201efbc77f4a921f0f5d6b7acf7e8d1
Fabricio Juinor de Campos <fabriciogit2024@hotmail.com>
2026-07-28 15:47:43 -0300

    inicio da configuracao de erro

---
+++ src/main/java/br/com/noticiarioOficial/model/ErrorType.java
@@ -0,0 +1,36 @@
+package br.com.noticiarioOficial.model;
+
+public class ErrorType {
+
+	private String message;
+	private String code;
+	private String error;
+	private String staus;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getStaus() {
+		return staus;
+	}
+	public void setStaus(String staus) {
+		this.staus = staus;
+	}
+	
+	
+
+}
